d2e37de3cd5e73cb8c766f27ea2dc12872e69266
JackMal157 <malates5@msu.edu>
2025-04-07 03:08:45 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,75 @@
-import { useState } from 'react'
+import React from 'react';
 import './App.css'
+import logo from './logo.svg';
+
+const magazines = [
+  { id: 1, title: 'Fenway Park', theme: 'architecture', isAvailable: true },
+  { id: 2, title: 'Mikes Pastry', theme: 'architecture', isAvailable: true },
+  { id: 3, title: 'Faneuil Haul', theme: 'design', isAvailable: false },
+];
+
+function ZineRack() {
+  const listZines = magazines.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'orange' : 'green'
+      }}
+    >
+      {zine.title}
+    </li>
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
 
 const book = {
   title: 'TD Garden',
   location: 'Boston, MA',
-  image: 
-  
+  credit: 'Photo taken by Jack Malatesta',
+  image: logo,
+  width: '264',
+  height: '378'
+};
+
+function Bookshelf() {
+  return (
+    <div>
+      <h2>{book.title} </h2>
+      <h3>{book.location}</h3>
+      {book.image &&
+      <img
+        className="bookCover"
+        src={book.image}
+        alt={book.title + ' cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+      />
+      }
+      <p>{book.credit}</p>
+    </div>
+  );
 }
 
 function MagicButton() {
   return (
-    <button>Click here</button>
+    <>
+    <h3>This button will take you to nothing!</h3>
+    <button>Click Here</button>
+    </>
   );
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Jack's React Site!!!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <MagicButton/>
+    <h1>Places to Visit in Boston</h1>
+    <ZineRack/>
+    <Bookshelf/>
+    <MagicButton/>
     </>
   )
 }
